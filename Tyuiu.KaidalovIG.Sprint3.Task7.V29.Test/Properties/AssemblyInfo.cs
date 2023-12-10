b1f04002119f1009d0ab7b98eece0e53dcd2dd48
Tyuiu.KaidalovIG@outlook.com <Kaydalov.2026@mail.ru>
2023-12-10 22:16:42 +0500

    Task7

---
+++ Tyuiu.KaidalovIG.Sprint3.Task7.V29.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.KaidalovIG.Sprint3.Task7.V29.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.KaidalovIG.Sprint3.Task7.V29.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("302a1f1d-e327-46d7-95d4-669d8a1509eb")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.KaidalovIG.Sprint3.Task7.V29/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.KaidalovIG.Sprint3.Task7.V29.Lib;
+
+namespace Tyuiu.KaidalovIG.Sprint3.Task7.V29
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #3 | Выполнил: Кайдалов И. Г. | СМАРТб-23-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("* Тема: Добавление к решению итоговых проектов по спринту                 *");
+            Console.WriteLine("* Задание #7                                                              *");
+            Console.WriteLine("* Вариант #29                                                             *");
+            Console.WriteLine("* Выполнил: Кайдалов Илья Григорьевич | СМАРТб-23-1                       *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Значения занести в массив.                                              *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int startValue = -5;
+            int stopValue = 5;
+
+            Console.WriteLine("Старт шага :" + startValue);
+            Console.WriteLine("Конец шага :" + stopValue);
+
+            int len = (stopValue - startValue) + 1;
+            double[] valueWaitArray;
+            valueWaitArray = new double[len];
+            valueWaitArray = ds.GetMassFunction(startValue, stopValue);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("+---------+---------+");
+            Console.WriteLine("|    X    |   f(x)  |");
+            Console.WriteLine("+---------+---------+");
+            for (int i = 0; i <= len - 1; i++)
+            {
+                Console.WriteLine("|{0,5:d}    | {1, 6:f2}  |", startValue, valueWaitArray[i]);
+                startValue++;
+            }
+
+            Console.WriteLine("+---------+---------+");
+
+            Console.ReadKey();
+        }
+    }
+}
