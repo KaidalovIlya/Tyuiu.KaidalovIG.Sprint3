917f880ed8485fdce5e8dde0bf494303df8b6657
Tyuiu.KaidalovIG@outlook.com <Kaydalov.2026@mail.ru>
2023-12-10 21:59:05 +0500

    Task5

---
+++ Tyuiu.KaidalovIG.Sprint3.Task5.V16.Test/DataServiceTest.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.KaidalovIG.Sprint3.Task5.V16.Lib;
+
+namespace Tyuiu.KaidalovIG.Sprint3.Task5.V16.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidGetSumTheDivisors()
+        {
+            DataService ds = new DataService();
+
+            int startValue = 17;
+            int stopValue = 26;
+
+            int res = ds.GetSumTheDivisors(startValue, stopValue);
+            int wait = 41;
+
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
